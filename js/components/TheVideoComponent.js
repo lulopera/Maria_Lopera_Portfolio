ab35ea90fd3c20e3efeffdacaa8abecd221c4eec
Lulopera <lulopera@hotmail.com>
2021-11-30 22:10:55 -0500

    adding clear

--- js/components/TheVideoComponent.js
+++ js/components/TheVideoComponent.js
@@ -3,10 +3,9 @@ export default {
 
     template: `
          <section class="video-player">
-            <i class="fas fa-film media-icon"></i><h1>This is the video component</h1>
+            <i class="fas fa-film media-icon"></i><h1></h1>
             <video src="" controls></video>
-            <h3>Video Title</h3>
-            <h3>Video Release Date</h3>
-            <p>video description here...</p>
-        </section>`
+
+        </section>
+        `
 }
